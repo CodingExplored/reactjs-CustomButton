f648abf25dd901bd52346c8ee28dc4fa0643eaf3
CodingExplored <144565131+CodingExplored@users.noreply.github.com>
2023-09-17 02:40:27 -0500

    Creating component and starter styles

--- components/CustomButton.js
+++ components/CustomButton.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './styles/CustomButton.css'
 
-const CustomButton = ({}) => {
+const CustomButton = ({ label, onClick, variant, size, disabled }) => {
+  let classNames = `customButton ${CustomButton}--variant ${CustomButton}--size ${disabled ? 'CustomButton--disabled' : ''}` ;
+
   return (
       <div>
+        <button className={classNames} onClick={onClick} disabled={disabled}>
+          {label}
+        </button>
       </div>
     );
 }
